89910aafa6a9a729d5d366b9216930a4c1bba02c
michealroberts <michael@observerly.com>
2025-02-24 17:16:09 +0000

    feat: add ZWOASIIOError in errors to zwoasi module
    feat: add ZWOASIIOError in errors to zwoasi module

--- src/zwoasi/errors.py
+++ src/zwoasi/errors.py
@@ -5,6 +5,10 @@
 
 # **************************************************************************************
 
+from typing import Optional
+
+# **************************************************************************************
+
 
 class ZWOASIError(Exception):
     """
@@ -13,6 +17,22 @@ class ZWOASIError(Exception):
 
     def __init__(self, message: str) -> None:
         super().__init__(message)
+
+
+# **************************************************************************************
+
+
+class ZWOASIIOError(ZWOASIError):
+    """
+    Exception class for all errors returned from the ASI SDK library.
+
+    :param message: A descriptive error message.
+    :param error_code: An optional integer error code returned by the SDK.
+    """
+
+    def __init__(self, message: str, error_code: Optional[int] = None) -> None:
+        super().__init__(message)
+        self.error_code = error_code
 
 
 # **************************************************************************************
