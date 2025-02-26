6c514f03c799d1b54f39d4b29add2e1e613ad283
michealroberts <michael@observerly.com>
2025-02-26 20:00:21 +0000

    feat: add errors ZWOASIIOError mapping to errors in zwoasi module
    feat: add errors ZWOASIIOError mapping to errors in zwoasi module

--- src/zwoasi/errors.py
+++ src/zwoasi/errors.py
@@ -56,3 +56,36 @@ class ZWOASIExposureError(ZWOASIError):
 
 
 # **************************************************************************************
+
+errors = [
+    None,
+    ZWOASIIOError("Invalid index", ZWOASIErrorCode.INVALID_INDEX),
+    ZWOASIIOError("Invalid ID", ZWOASIErrorCode.INVALID_ID),
+    ZWOASIIOError("Invalid control type", ZWOASIErrorCode.INVALID_CONTROL_TYPE),
+    ZWOASIIOError("Camera closed", ZWOASIErrorCode.CAMERA_CLOSED),
+    ZWOASIIOError("Camera removed", ZWOASIErrorCode.CAMERA_REMOVED),
+    ZWOASIIOError("Invalid path", ZWOASIErrorCode.INVALID_PATH),
+    ZWOASIIOError("Invalid file format", ZWOASIErrorCode.INVALID_FILEFORMAT),
+    ZWOASIIOError("Invalid size", ZWOASIErrorCode.INVALID_VIDEO_SIZE),
+    ZWOASIIOError("Invalid image type", ZWOASIErrorCode.INVALID_IMAGE_TYPE),
+    ZWOASIIOError(
+        "Outside of boundary", ZWOASIErrorCode.START_POSITION_OUT_OF_BOUNDARY
+    ),
+    ZWOASIIOError("Timeout", ZWOASIErrorCode.TIMEOUT),
+    ZWOASIIOError("Invalid sequence", ZWOASIErrorCode.INVALID_SEQUENCE),
+    ZWOASIIOError("Buffer too small", ZWOASIErrorCode.BUFFER_TOO_SMALL),
+    ZWOASIIOError("Video mode active", ZWOASIErrorCode.VIDEO_MODE_ACTIVE),
+    ZWOASIIOError("Exposure in progress", ZWOASIErrorCode.EXPOSURE_IN_PROGRESS),
+    ZWOASIIOError("General error", ZWOASIErrorCode.GENERAL_ERROR),
+    ZWOASIIOError("Invalid mode", ZWOASIErrorCode.INVALID_MODE),
+    ZWOASIIOError("GPS not supported", ZWOASIErrorCode.GPS_NOT_SUPPORTED),
+    ZWOASIIOError("Invalid GPS version", ZWOASIErrorCode.INVALID_GPS_VERSION),
+    ZWOASIIOError("Invalid GPS FPGA", ZWOASIErrorCode.INVALID_GPS_FPGA),
+    ZWOASIIOError(
+        "Invalid GPS parameter out of range",
+        ZWOASIErrorCode.INVALID_GPS_PARAM_OUT_OF_RANGE,
+    ),
+    ZWOASIIOError("Invalid GPS data", ZWOASIErrorCode.INVALID_GPS_DATA),
+]
+
+# **************************************************************************************
